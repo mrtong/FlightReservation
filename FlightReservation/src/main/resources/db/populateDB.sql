7bb208b6a1dff7f061b1a800000acf7ed5f25602
mrtong <mrtong2009@gmail.com>
2013-06-22 17:22:47 +1000

    init

---
+++ FlightReservation/src/main/resources/db/populateDB.sql
@@ -0,0 +1,6 @@
+insert into AIRPORT(CODE,NAME,CITY) values('SYD', 'Sydney International', 'Sydney');
+
+insert into AIRPORT(CODE,NAME,CITY) values('MEL', 'Melbourn International', 'Melbourn');
+insert into AIRPORT(CODE,NAME,CITY) values('HK', 'Hong Kong International', 'HK');
+
+insert into FLIGHT(NUMBER,FROM_AIRPORT_CODE,TO_AIRPORT_CODE,MILES,DEPARTURE_TIME,SEATS_AVAILABLE) values('HK 234', 'SYD','HK',8,'2013-03-01 20:08',120);

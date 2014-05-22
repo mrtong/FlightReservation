0b73dfd71955b913f554d615fdf1d47d5faff10c
mrtong <mrtong2009@gmail.com>
2014-05-22 15:11:48 +1000

    AirportService test

--- FlightReservation/src/test/resources/com/foo/flight/service/AirportServiceTest.java
+++ FlightReservation/src/test/resources/com/foo/flight/service/AirportServiceTest.java
@@ -1,52 +1,40 @@
 package com.foo.flight.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import com.foo.flight.dao.jpa.JpaAirportDaoImpl;
+import com.foo.flight.dao.jpa.JpaDefaultDaoConfig;
 import com.foo.flight.model.Airport;
-import com.foo.flight.service.interfaces.AirportService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {MockDaoConfig.class,
-    DefaultServiceConfig.class }, loader = AnnotationConfigContextLoader.class)
+@ContextConfiguration(classes = { JpaDefaultDaoConfig.class }, loader = AnnotationConfigContextLoader.class)
 public class AirportServiceTest {
-  @Autowired
-  private JpaAirportDaoImpl airportDaoMock;
+	@Autowired
+	private JpaAirportDaoImpl airportDaoMock;
 
-  @Autowired
-  private ApplicationContext ctx;
+	@Test
+	public void getAirportByCode() throws Exception {
+		String code = "LAX";
+		Airport airport = airportDaoMock.findOne(code);
+		assertNotNull(airport);
+		assertEquals("Los Angeles International", airport.getName());
+	}
 
-  @Test
-  public void getAirportCode() throws Exception {
-    String code = "LAX";
-    Airport airport = new Airport();
-
-//    Mockito.when(airportDaoMock.getAirport(code)).thenReturn(airport);
-    AirportService a = ctx.getBean(AirportService.class);
-    assertTrue(airport == a.getAirport(code));
-  }
-  
-  @Test
-  public void getAirports() {
-    List<Airport> airports = new ArrayList<Airport>();
-    for (int i = 0; i < 10; i++) {
-      Airport a = new Airport("Code:" + i, "Name:" + i, "City:" + i);
-      airports.add(a);
-    }
-//    Mockito.when(airportDaoMock.getAirports()).thenReturn(airports);
-    assertEquals(airports, ctx.getBean(AirportService.class).getAirports());
-  }
+	@Test
+	public void getAirports() {
+		List<Airport> airports = airportDaoMock.findAll();
+		assertNotNull(airports);
+		assertTrue(airports.size() > 0);
+	}
 }
-

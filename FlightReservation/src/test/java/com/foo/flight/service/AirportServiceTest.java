b4bc22acbc88bf8b448fd50989bb9c484e0a8245
mrtong <mrtong2009@gmail.com>
2014-07-29 15:51:58 +1000

    Airport service tests and integration tests

--- FlightReservation/src/test/java/com/foo/flight/service/AirportServiceTest.java
+++ FlightReservation/src/test/java/com/foo/flight/service/AirportServiceTest.java
@@ -1,79 +1,46 @@
 package com.foo.flight.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import java.util.List;
-
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.foo.flight.dao.jpa.JpaAirportDaoImpl;
-import com.foo.flight.model.Airport;
-import com.foo.flight.model.support.AirportBuilder;
 import com.foo.flight.service.exceptions.NoSuchAirportException;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
 public class AirportServiceTest {
 	
-	@Configuration
-	static class AirportServiceTestContextConfiguration {
-		@Bean
-		public AirportServiceImpl airlineService() {
-			return new AirportServiceImpl();
-		}
-
-		@Bean
-		public JpaAirportDaoImpl flightDao() {
-			return Mockito.mock(JpaAirportDaoImpl.class);
-		}
-	}
-	@Autowired
-	private AirportServiceImpl airportServiceMock;
+	Logger log = Logger.getLogger(AirportServiceTest.class);
 	
-	@Autowired
-	private JpaAirportDaoImpl airportDaoMock;
+	private AirportServiceImpl airportService;
+	
+	@Mock
+	private JpaAirportDaoImpl airportDao;
 	
 	@Before
 	public void setup() {
-		Airport airport = new AirportBuilder(){
-			{
-				buildAirport("LAX","Los Angeles International","Los Angeles");
-			}
-			
-		}.build(true);
+		
+		MockitoAnnotations.initMocks( this );
+		airportService=Mockito.spy(new AirportServiceImpl());
+		ReflectionTestUtils.setField(airportService, "airportDao", airportDao);
 
-		Mockito.when(airportDaoMock.findOne("LAX")).thenReturn(airport);
-	}
-	
-
-
-	@Test
-	public void getAirportByCode() throws Exception {
-		String code = "LAX";
-		Airport airport = airportServiceMock.getAirport(code);
-		assertNotNull(airport);
-		assertEquals("Los Angeles International", airport.getName());
 	}
 	
 	@Test(expected=NoSuchAirportException.class)
-	public void getAirportByCodeNotFound() throws Exception {
+	public void getAirportByCode() throws Exception {
+		log.info("TEST:::getAirportByCode");
 		String code = "LAX123";
-		Airport airport = airportServiceMock.getAirport(code);
-		//assertEquals("Los Angeles International", airport.getName());
+		airportService.getAirport(code);
+		Mockito.verify(airportDao).findOne(code);
 	}
 	
 	@Test
 	public void getAirports() {
-		List<Airport> airports = airportServiceMock.getAirports();
-		assertNotNull(airports);
+		log.info("TEST:::getAirports");
+		airportService.getAirports();
+		Mockito.verify(airportDao).findAll();
 	}
 }
